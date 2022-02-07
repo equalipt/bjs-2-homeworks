ae5b539c73cd33dcae9cfddc5d6b7944a5239974
Dmitriy <semenchenkodmitriy@gmail.com>
2022-02-07 19:30:25 +0300

    func task

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,67 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = arr[0];
+  max = arr[0];
+  sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (min > arr[i]) {
+        min = arr[i];
+    }
 
-  return { min: min, max: max, avg: avg };
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+
+    sum += arr[i];
+    avg = sum / arr.length;
+  }
+  
+  
+  return { min: min, max: max, avg: +avg.toFixed(2) };
 }
 
 // Задание 2
-function worker(arr) {
-  let sum;
 
-  // Ваш код
+function worker(arr) {
+  let sum = 0;
+
+  for (let item of arr) {
+    sum += item;
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
 
-  // Ваш кода
-  // for ...
-  
+  for (let item of arrOfArr) {
+    const worker = func(item);
+
+    if (worker > max) {
+      max = worker;
+    }
+  }
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+  let max = arr[0];
+  let dif = 0;
+
+  for (let item of arr) {
+    if (item > max) {
+      max = item;
+    }
+    if (item < min) {
+      min = item;
+    }
+  }
+
+  return dif = (max - min);
 }
