8505af5243b0ea99ba20ca133f63884cab770d1c
Dmitriy <semenchenkodmitriy@gmail.com>
2022-02-14 18:30:39 +0300

    fin

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,19 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+
+   if (d == 0) {
+    let x0 = -b / (2 * a);
+    arr.push(x0);
+    
+  } else if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1);
+    arr.push(x2);
+  }
+  
+  return arr; 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
