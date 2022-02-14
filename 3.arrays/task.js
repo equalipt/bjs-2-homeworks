c8b599f18203512ec2a3669072cd37f29218d0b2
Dmitriy <semenchenkodmitriy@gmail.com>
2022-02-14 18:24:14 +0300

    fin

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
+// task 1
+
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  
+  let result = arr1.length === arr2.length && arr1.every((item, idx) => arr2[i] === item);
+  
+  return result;
 }
 
+// task 2
+
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  
+  let resultArr = arr.filter(item => item > 0).filter(item => item % 3 == 0).map(item => item * 10)
+  
   return resultArr; // array
 }
