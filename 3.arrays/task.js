da80772a04362a1fd31de4da63cbe79a5f752b22
Dmitriy <semenchenkodmitriy@gmail.com>
2022-02-19 14:35:54 +0300

    let result to return result

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,17 +1,11 @@
 // task 1
 
 function compareArrays(arr1, arr2) {
-  
-  let result = arr1.length === arr2.length && arr1.every((item, idx) => arr2[i] === item);
-  
-  return result;
+  return arr1.length === arr2.length && arr1.every((item, idx) => arr2[i] === item);
 }
 
 // task 2
 
 function advancedFilter(arr) {
-  
-  let resultArr = arr.filter(item => item > 0).filter(item => item % 3 == 0).map(item => item * 10)
-  
-  return resultArr; // array
+  return arr.filter(item => item > 0).filter(item => item % 3 == 0).map(item => item * 10);
 }
